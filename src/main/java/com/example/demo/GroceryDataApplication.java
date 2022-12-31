e281342a2b1e90805c6c6ce2d8b5b114590c34e5
chiragsoni2002 <sonichirag.sc.345@gmail.com>
2022-12-31 13:00:17 +0530

    Master Branch

---
+++ src/main/java/com/example/demo/GroceryDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryDataApplication.class, args);
+	}
+
+}
